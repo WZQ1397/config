1d0cbc6b0ed99f0225c8d4ea204950043ba9649c
WZQ1397 <wzqsergeant@vip.qq.com>
2019-08-01 09:35:46 +0800

    Update consul-deregister.sh

--- Kubernetes/yml/tsjr/consul/script/consul-deregister.sh
+++ Kubernetes/yml/tsjr/consul/script/consul-deregister.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-for x in `curl http://172.16.48.111:8500/v1/agent/services | jq 'keys' | jq .[] | cut -d '"' -f 2`
-do 
-curl -X PUT http://172.16.48.111:8500/v1/agent/service/deregister/$x
+url="http://consul.k8s-dev-local.tsjinrong.top"
+for x in `curl http://consul.k8s-dev-local.tsjinrong.top/v1/agent/services | jq 'keys' | jq .[] | cut -d '"' -f 2`
+do
+curl -X PUT http://consul.k8s-dev-local.tsjinrong.top/v1/agent/service/deregister/$x
 done
 
 # 单独删除服务
