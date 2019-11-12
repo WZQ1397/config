dd2859065f219935077aedf16817b4c473f7a770
WZQ1397 <wzqsergeant@vip.qq.com>
2019-11-12 10:31:51 +0800

    node

--- Kubernetes/yml/node/node-migrate.sh
+++
@@ -1,9 +0,0 @@
-# HOW TO CHANGE NODE
-# Step 1. get node information
-kubectl get node
-# Step 2. set node unschedule
-kubectl cordon $node_name
-# Step 3. schedul all services of this node to other
-kubectl cordon $node_name
-# Step 4. delete node 
-kubectl delete $node_name
