8f232bdafa3a4f814d2d2872d208ebb6aa591997
WZQ1397 <wzqsergeant@vip.qq.com>
2020-04-01 14:16:08 +0800

    Revert "tf-aws-ec2"
    This reverts commit eca745295daea7ff31f2becd32010ae676aaf8cb.

--- terraform/tf-aws-alicloud-instance-prd/tf-aws-ec2/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "consul" {
-    address = "consul.k8s-dev-local.tsjinrong.top"
-    scheme  = "http"
-    path    = "tf/state"
-    # token   = ""
-  }
-}
-
--- terraform/tf-aws-alicloud-instance-prd/tf-aws-ec2/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region = "cn-north-1"
-    access_key = "AKIAPTL3BCBLCTPWV4HQ"
-    secret_key = "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"
-    #shared_credentials_file  = "/root/.aws/credentials"
-    profile = "default"
-}
-
