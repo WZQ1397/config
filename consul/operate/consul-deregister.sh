4fe1b1bc287d87440802cf4bdc0aa857a396c7aa
WZQ1397 <wzqsergeant@vip.qq.com>
2020-06-04 18:10:05 +0800

    Update consul-deregister.sh

--- consul/operate/consul-deregister.sh
+++ consul/operate/consul-deregister.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for x in `curl http://172.16.48.111:8500/v1/agent/services | jq 'keys' | jq .[] | cut -d '"' -f 2`
+consulAddr=172.30.197.185
+for x in `curl http://$consulAddr:8500/v1/agent/checks | jq '.[] | select(.Status != "passing") | .CheckID' | awk -F ":" 'gsub(/"/,"",$0){print $NF}'`
 do 
-curl -X PUT http://172.16.48.111:8500/v1/agent/service/deregister/$x
+curl -X PUT http://$consulAddr:8500/v1/agent/service/deregister/$x
 done
+
+# 单独删除服务
+# for x in `curl http://172.30.106.149:8500/v1/agent/services | jq 'keys' | jq .[] | cut -d '"' -f 2 | grep appro ` ; do curl -X PUT http://172.30.106.149:8500/v1/agent/service/deregister/$x; done
